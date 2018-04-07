ed3845fc61177f5a810565e5a035bf5890f0ba15
Lior Baraban <oracleilb@gmail.com>
2018-04-07 18:26:18 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 600,
+        height: 600,
+        backgroundColor: '#ffffff',
+        icon: `file://${__dirname}/dist/assets/logo.png`
+    });
+
+    win.loadURL(`file://${__dirname}/dist/index.html`);
+
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+
+    // On macOS specific close process
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', function () {
+    // macOS specific close process
+    if (win === null) {
+        createWindow()
+    }
+})
